921838720eb2326e156cfee973476650905f8942
nitin55220 <ankur55420@gmail.com>
2020-08-19 16:37:29 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import HomePage from './pages/homepage/homepage.componenets'
 import './App.css';
-import {Route} from 'react-router-dom'
+import {Route,Switch} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
-
+import Header from './componenets/Header/header.component'
 function dummy(){
   return <h1>test</h1>
 }
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
-
+      </Switch>
     </div>
   );
 }
