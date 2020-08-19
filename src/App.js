af93f4e125e6c7b58bb2d5813035ff6711c6fd88
nitin55220 <ankur55420@gmail.com>
2020-08-19 15:39:00 +0530

    added shop page collection priview and collection items

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import HomePage from './pages/homepage/homepage.componenets'
 import './App.css';
 import {Route} from 'react-router-dom'
-
+import ShopPage from './pages/shop/shop.component'
 
 function dummy(){
   return <h1>test</h1>
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={dummy} />
+      <Route path='/shop' component={ShopPage} />
 
     </div>
   );
