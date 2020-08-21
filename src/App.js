f2dd0892662314cc1c173131a51966e36cc96131
nitin55220 <ankur55420@gmail.com>
2020-08-21 16:41:25 +0530

    created folder to store action types strings

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import HomePage from './pages/homepage/homepage.componenets'
 import './App.css';
-import {Route,Switch} from 'react-router-dom'
+import {Route,Switch,Redirect} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import Header from './componenets/Header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
@@ -45,7 +45,7 @@ class App extends React.Component{
         <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
+        <Route exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to='/' />):(<SignInAndSignUpPage />)} />
   
   
         </Switch>
@@ -55,7 +55,11 @@ class App extends React.Component{
  
 }
 
+const mapStateToProps = ({user})=>({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user=> dispatch(setCurrentUser(user))
 })
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
