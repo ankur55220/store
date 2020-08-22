36e5fd02a0be003b3f1f4a0b78243027c9759b34
nitin55220 <ankur55420@gmail.com>
2020-08-23 00:12:18 +0530

    completed the add cart drop down frontend

--- src/componenets/cart-dropdwn/cart-dropdown.component.jsx
+++ src/componenets/cart-dropdwn/cart-dropdown.component.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 
+import CartItem from '../cart-item/cart-item.component'
+import {connect} from 'react-redux'
 import './cart-dropdown.styles.scss'
 import CustomButton from '../../componenets/custom-button/custom-button.component'
 
-const CartDropdown = () =>(
+const CartDropdown = ({cartItems}) =>{
+     console.log(cartItems)
+    return (
     <div className='cart-dropdown'>
-        <div className='cart-item' />
+        <div className='cart-item' >
+        {
+                cartItems.map(cartItem=>(
+                    <CartItem key={cartItem.id} item={cartItem} />
+                ))
+            }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
-);
+)};
 
+const mapStateToProps=(state)=>({
+  cartItems:state.cart.cartItems
+})
 
-export default CartDropdown;
+export default connect(mapStateToProps)(CartDropdown);
